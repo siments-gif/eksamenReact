c2ae9b1b6fe784cfcc2ffb53ab025bbd351f3596
Simen Tveitane Skogen <simen.skogen@gokstadakademiet.no>
2024-12-02 13:45:31 +0100

    Added more folders felt i needed for project. Will change later

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <Button variant='contained' color='secondary' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is now {count}
         </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
